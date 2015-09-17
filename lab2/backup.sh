8d33552241e5e47a60bf35a004a535295b354fa5
ChowTheProgrammer <zhou267@purdue.edu>
2015-09-16 22:56:36 -0400

    Done with all functional requirements for backup.sh

--- lab2/backup.sh
+++ lab2/backup.sh
@@ -1,1 +1,35 @@
 #!/bin/bash
+Secs=$3
+
+fileName="$2/$(date +"%Y-%m-%d-%H-%M-%S")-$1"
+for i in `seq 1 $4`
+do
+    history[$i]=""
+done
+
+
+cp $1 $fileName                           #copy file into backup dir
+count=1
+let mod=($count)%$4
+history[$mod]=$fileName                   #store first file name into
+                                          #history array
+
+
+while true; do
+    sleep $Secs
+
+    if ! cmp -s $1 $fileName; then
+        # echo "Difference detected"
+        echo `diff $1 $fileName` > diff.txt
+        /usr/bin/mailx -s "Backup" $USER < diff.txt
+        latest="$2/$(date +"%Y-%m-%d-%H-%M-%S")-$1"
+        cp $1 $latest
+        let count=$count+1
+        let mod=($count)%$4
+        fileName=$latest
+        if [[ $count -gt $4 ]]; then          #check if need to delete file
+            rm "${history[$mod]}"               #before save its name to history
+        fi
+        history[$mod]=$fileName               #save the latest file name into
+    fi
+done
