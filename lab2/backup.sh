5fbb278bc8b07fc7e639a56b0c82764c9de4d8ef
ChowTheProgrammer <zhou267@purdue.edu>
2015-09-17 00:08:01 -0400

    comments added

--- lab2/backup.sh
+++ lab2/backup.sh
@@ -6,19 +6,28 @@ FileName="$2/$(date +"%Y-%m-%d-%H-%M-%S")-$1"
 cp $1 $FileName
 Count=1
 Mod=($Count)%$4
+# create a history array
 History[$Mod]=$FileName
 
 while true; do
     sleep $Secs
 
     if ! cmp -s $1 $FileName; then
+
+        # email user the difference
         echo `diff $1 $FileName` > diff.txt
         /usr/bin/mailx -s "Backup" $USER < diff.txt
+        rm diff.txt
         latest="$2/$(date +"%Y-%m-%d-%H-%M-%S")-$1"
+
         cp $1 $latest
         let Count=$Count+1
         let Mod=($Count)%$4
         FileName=$latest
+
+        # if the count reaches the limit,
+        # then update the oldest backup with
+        # the latest one.
         if [ $Count -gt $4 ]; then
             rm "${History[$Mod]}"
         fi
